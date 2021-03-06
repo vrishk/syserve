744d5c5b3695e45b231dfcb9919a14b65ff8bf98
vrishk <krishnavrishab@gmail.com>
2021-03-06 12:18:24 +0530

    chore: readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# SYserve
+
+**S**imple **Y**ew **Serve**r made with [`actix_web`](https://actix.rs/).
+
+Implemented from the [actix example server in the yew-router repo](https://github.com/yewstack/yew_router/tree/master/examples/servers)
+with a few changes.
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,6 @@ use actix_web::{get, middleware, web, App, Error, HttpResponse, HttpServer};
 // You will need to change this if you use this as a template for your application.
 const ASSETS_DIR: &str = "../personal/dist";
 
-#[get("/api")]
-async fn api_404() -> HttpResponse {
-    HttpResponse::NotFound().finish()
-}
-
-#[get("/api/{unconfigured_routes:.*}")]
-async fn api_404_unconfigured() -> HttpResponse {
-    HttpResponse::NotFound().finish()
-}
-
-#[get("/api/hello/{name}")]
-async fn api_hello(name: web::Path<String>) -> HttpResponse {
-    HttpResponse::Ok().body(name.into_inner())
-}
-
 async fn serve_index_html() -> Result<NamedFile, Error> {
     const INDEX_HTML: &str = "index.html";
     let index_file = format!("{}/{}", ASSETS_DIR, INDEX_HTML);
@@ -38,11 +23,6 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
-            .service(api_404)
-            .service(api_hello)
-            // Important this comes last so all configured api routes will match
-            // before this catch all
-            .service(api_404_unconfigured)
             .service(actix_files::Files::new("/", ASSETS_DIR).index_file("index.html"))
             .default_service(web::get().to(serve_index_html))
     })
