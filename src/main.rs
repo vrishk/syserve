70bb4f72a2fe45a8fbd126c3d463af3e5e0e81af
vrishk <krishnavrishab@gmail.com>
2021-03-08 18:13:11 +0530

    feat: dir as cli arg

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,27 @@
 use actix_files::NamedFile;
 use actix_web::{get, middleware, web, App, Error, HttpResponse, HttpServer};
 
-// You will need to change this if you use this as a template for your application.
-const ASSETS_DIR: &str = "../personal/dist";
-
-async fn serve_index_html() -> Result<NamedFile, Error> {
-    const INDEX_HTML: &str = "index.html";
-    let index_file = format!("{}/{}", ASSETS_DIR, INDEX_HTML);
-
-    Ok(NamedFile::open(index_file)?)
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_server=info,actix_web=info");
     env_logger::init();
 
-    let localhost: &str = "0.0.0.0";
-    let port: u16 = 8000;
-    let addr = (localhost, port);
+    // Constants
+    const ADDR: (&str, u16) = ("0.0.0.0", 8000);
+
+    let args: Vec<String> = std::env::args().collect();
+    let dir = args[1].clone();
 
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
-            .service(actix_files::Files::new("/", ASSETS_DIR).index_file("index.html"))
-            .default_service(web::get().to(serve_index_html))
+            .service(actix_files::Files::new("/", &dir).index_file("index.html"))
+            .default_service(actix_files::Files::new(
+                "/",
+                format!("{}/{}", dir, "index.html"),
+            ))
     })
-    .bind(addr)?
+    .bind(ADDR)?
     .workers(1)
     .run()
     .await
