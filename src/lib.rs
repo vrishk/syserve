f7e38b390fbce7bb59aec3284142e89d7f64defc
vrishk <krishnavrishab@gmail.com>
2021-03-13 21:41:22 +0530

    feat: args checking

--- src/lib.rs
+++ src/lib.rs
@@ -5,16 +5,36 @@ use warp::Filter;
 #[derive(Clone)]
 pub struct Args {
     dir: String,
+    index: String,
 }
 
 impl Args {
     pub fn new() -> Args {
         Args {
             dir: String::from("."),
+            index: String::from("index.html"),
         }
     }
 
-    pub fn parse(&mut self) {
+    pub fn parse(&mut self) -> Result<(), &str> {
+        self.store();
+        self.verify()
+    }
+
+    fn verify(&self) -> Result<(), &str> {
+        // Dir check
+        if !PathBuf::from(&self.dir).is_dir() {
+            return Err("Directory does not exist");
+        };
+        // Index check
+        if !PathBuf::from(&self.dir).join(&self.index).is_file() {
+            return Err("Index file does not exist");
+        };
+
+        Ok(())
+    }
+
+    fn store(&mut self) {
         let mut parser = ArgumentParser::new();
         parser.set_description("Server for yew and yew-router apps");
 
@@ -23,6 +43,12 @@ impl Args {
             Store,
             "Directory to serve (must contain index.html)",
         );
+        parser.refer(&mut self.index).add_option(
+            &["-i", "--index"],
+            Store,
+            "Index file to be served",
+        );
+
         parser.parse_args_or_exit();
     }
 }
@@ -38,7 +64,7 @@ pub async fn server(args: Args) {
         .boxed();
 
     let files = assets.or(warp::get()
-        .and(warp::fs::file(PathBuf::from(args.dir).join("index.html")))
+        .and(warp::fs::file(PathBuf::from(args.dir).join(args.index)))
         .boxed());
 
     warp::serve(warp::any().and(files).boxed()).run(addr).await;
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,11 @@ use syserve::{server, Args};
 #[tokio::main]
 async fn main() {
     let mut args = Args::new();
-    args.parse();
+
+    if let Err(s) = args.parse() {
+        eprintln!("Error parsing arguments: {}", s);
+        std::process::exit(1);
+    }
 
     server(args).await;
 }
